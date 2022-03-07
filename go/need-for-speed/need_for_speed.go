4dc7578420843a3fe240c7bd9e7d0f3fb82ae8dc
Angelo Marano <angelo@keyshake.io>
2022-03-07 23:05:18 +0000

    Added blackjack golang solution

---
+++ go/need-for-speed/need_for_speed.go
@@ -0,0 +1,42 @@
+package speed
+
+// TODO: define the 'Car' type struct
+
+type Car struct {
+	speed        int
+	batteryDrain int
+	battery      int
+	distance     int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{speed: speed, batteryDrain: batteryDrain, battery: 100}
+}
+
+// TODO: define the 'Track' type struct
+
+type Track struct {
+	distance int
+}
+
+// NewTrack created a new track
+func NewTrack(distance int) Track {
+	return Track{distance: distance}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive on more time,
+// the car will not move.
+func Drive(car Car) Car {
+	if car.battery < car.batteryDrain {
+		return car
+	}
+
+	return Car{speed: car.speed, batteryDrain: car.batteryDrain, battery: car.battery - car.batteryDrain, distance: car.distance + car.speed}
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	remaining_distance := car.speed * (car.battery / car.batteryDrain)
+	return remaining_distance >= track.distance
+}
